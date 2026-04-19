fc0b066f2ebf66148514ccb3320e1ee967164ff2
viveek-sh <get.viveksahu@gmail.com>
2026-04-19 21:43:08 +0530

    feat: add MDX support and Tailwind Typography integration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["10.11.12.10"],
+  pageExtensions: ["mdx", "ts", "tsx"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      { protocol: "http", hostname: "images.pexels.com" },
+    ],
+  },
 };
 
 export default nextConfig;
