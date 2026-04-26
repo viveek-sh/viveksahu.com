b8bd06f8c6d6286cb528a92b96bdc5b89ed656db
viveek-sh <get.viveksahu@gmail.com>
2026-04-26 14:14:06 +0530

    fix: minor changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["10.11.12.10"],
   pageExtensions: ["mdx", "ts", "tsx"],
   output: "export",
   images: {
